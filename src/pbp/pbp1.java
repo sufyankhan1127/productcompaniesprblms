4541190fd5d8d9a8ca8bf4c82a89cd0f5eca91e2
Mohd Sufyan khan <sufyankhan.01127@gmail.com>
2026-08-27 02:27:37 +0530

    Created Student Class

--- src/pbp/pbp1.java
+++ src/pbp/pbp1.java
@@ -3,3 +3,86 @@ package pbp;
 public class pbp1 {
 
 }
+
+class Student{
+	private int id;
+	private String name;
+	private int age;
+	private String email;
+	private long phone;
+	private String course;
+	private String department;
+	private int marks;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	public Student(int id, String name, int age, String email, long phone, String course, String department,
+			int marks) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.email = email;
+		this.phone = phone;
+		this.course = course;
+		this.department = department;
+		this.marks = marks;
+	}
+	
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", age=" + age + ", email=" + email + ", phone=" + phone
+				+ ", course=" + course + ", department=" + department + ", marks=" + marks + "]";
+	}
+	
+	
+	
+}
